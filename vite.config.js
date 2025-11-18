04716fe1cfa49c7445c7d6fb79beff02ef72542e
Jagruti Londhe <jagrateelondhe@gmail.com>
2025-11-18 06:30:23 +0530

    Remove unnecessary code block formatting

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-```js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,4 +6,3 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 plugins: [react()],
 })
-```
